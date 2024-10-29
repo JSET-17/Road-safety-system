ec4e4f19832dc7288eb0b61b547ae86502971862
Juan Sebastián Escobar Tangarife <jsebastian-1996@hotmail.com>
2024-10-29 17:57:04 -0500

    Base
    Base del proyecto rss

---
+++ Road Safety System/src/main/java/system/rss/RoadSafetySystemApplication.java
@@ -0,0 +1,12 @@
+package system.rss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadSafetySystemApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RoadSafetySystemApplication.class, args);
+	}
+
+}
---
+++ Road Safety System/src/test/java/system/rss/RoadSafetySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package system.rss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadSafetySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
